2cac30b3c360cb4f21c7dbfb6c2be200833cd7bd
Lu Duke <dukelu999@gmail.com>
2026-01-25 21:44:15 +0700

    import

---
+++ import/main.js
@@ -0,0 +1,6 @@
+import {x,y, add,sub} from './test.js';
+
+console.log(x);
+console.log(y);
+console.log(add(2, 3));
+console.log(sub(5, 2));
---
+++ import/test.js
@@ -0,0 +1,10 @@
+export const x =10;
+export const y =20;
+
+export function add(a, b) {
+    return a + b;
+}
+
+export function sub(a, b){
+    return a - b; 
+}
