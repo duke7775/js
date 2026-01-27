9dbffde0294e9b2cb380c76c37a2cdcd7bbd1b17
Lu Duke <dukelu999@gmail.com>
2026-01-27 21:35:55 +0700

    test3.js

---
+++ test3.js
@@ -0,0 +1,10 @@
+var message = "Hello World";
+console.log(message);
+console.log(__dirname);
+console.log(__filename);
+console.log(module);
+console.log(module.filename);
+
+console.log(global);
+global.x = 100;
+console.log(x);
